c236c78ae5255acce3fdb9ce68be68ff5f20717e
GiacomoPirrone <giacomollpirrone@gmail.com>
2020-08-09 20:43:07 +1000

    Additon and creation of worker repository

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/repository/WorkerRepository.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/repository/WorkerRepository.java
@@ -1,2 +1,10 @@
-package com.scrumoftheearth.springbootapi.repository;public interface WorkerRepository {
+package com.scrumoftheearth.springbootapi.repository;
+
+import com.scrumoftheearth.springbootapi.model.Worker;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WorkerRepository extends CrudRepository<Worker, Long> {
+
+    @Override
+    Iterable<Worker> findAllById(Iterable<Long> iterable);
 }
