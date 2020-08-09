7697b453eb833c903523005d27f493d2d8caa5a6
GiacomoPirrone <giacomollpirrone@gmail.com>
2020-08-09 20:43:42 +1000

    Addition and creation of worker service

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/service/WorkerService.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/service/WorkerService.java
@@ -1,2 +1,18 @@
-package com.scrumoftheearth.springbootapi.service;public class WorkerService {
+package com.scrumoftheearth.springbootapi.service;
+
+import com.scrumoftheearth.springbootapi.model.Worker;
+import com.scrumoftheearth.springbootapi.repository.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkerService {
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    public Worker saveOrUpdateWorker(Worker worker){
+
+        //business logic
+        return workerRepository.save(worker);
+    }
 }
