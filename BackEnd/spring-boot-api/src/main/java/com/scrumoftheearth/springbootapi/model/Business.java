c584eb1221a036d8c8f777ecae4956018b701455
Adrian <s3721092@student.rmit.edu.au>
2020-08-13 23:27:10 +1000

    updated business model to match class diagram

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/Business.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/Business.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 // POJO for business
@@ -13,14 +15,42 @@ public class Business {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     // business ID
     private Long id;
+    // state of the business
+//    private BusinessBState businessBState;
+//    // List of workers *need to change to Worker object*
+//    private List<String> workerList;
     @NotBlank(message = "Business name is required")
     // name of business
     private String name;
-    @NotBlank(message = "Business services is required")
-    // services the business provides
-    private String service;
+    @NotBlank(message = "Business blurb is required")
+    // blurb the business provides
+    private String blurb;
+    @NotBlank(message = "Business description is required")
+    // description of the business
+    private String description;
+    @NotBlank(message = "Business address is required")
+    // address of the business
+    private String address;
+    @NotBlank(message = "Business contact number is required")
+    // contact info of the business
+    private String phoneNumber;
 
+    // blank constructor for production uses
     protected Business() {
+//        workerList = new ArrayList<>();
+//        businessBState = new BusinessBState();
+    }
+
+    // constructor for testing
+    public Business(long id,String name,String blurb,String description,String address,String phoneNumber){
+        this.id = id;
+//        businessBState = new BusinessBState();
+//        workerList = new ArrayList<>();
+        this.name = name;
+        this.blurb = blurb;
+        this.description = description;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
     // getters and setters
@@ -40,11 +70,51 @@ public class Business {
         this.name = busname;
     }
 
-    public String getService() {
-        return service;
+    public String getBlurb() {
+        return blurb;
     }
 
-    public void setService(String busservice) {
-        this.service = busservice;
+    public void setBlurb(String busservice) {
+        this.blurb = busservice;
+    }
+
+//    public BusinessBState getBusinessBState() {
+//        return businessBState;
+//    }
+//
+//    public void setBusinessBState(BusinessBState businessBState) {
+//        this.businessBState = businessBState;
+//    }
+//
+//    public List<String> getWorkerList() {
+//        return workerList;
+//    }
+//
+//    public void setWorkerList(List<String> workerList) {
+//        this.workerList = workerList;
+//    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
---
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/BusinessBState.java
@@ -0,0 +1,14 @@
+package com.scrumoftheearth.springbootapi.model;
+
+// Store the state of one business
+public class BusinessBState {
+    // Business this state belongs to
+    private Business business;
+    // business state
+    //private BusinessState businessState **for later**
+    // State message
+    private String messsage;
+
+    public BusinessBState() {
+    }
+}
