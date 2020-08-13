6e9afee84dfc534a51a8541ac183a7fd3465f55a
GiacomoPirrone <giacomollpirrone@gmail.com>
2020-08-13 18:29:07 +1000

    Addition of new model WorkerWState

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/WorkerWState.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/WorkerWState.java
@@ -1,2 +1,19 @@
-package com.scrumoftheearth.springbootapi.model;public class WorkerWState {
+package com.scrumoftheearth.springbootapi.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class WorkerWState {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Worker worker;
+    private WorkerState workerState;
+
+    public WorkerWState() {
+
+
+    }
+
 }
