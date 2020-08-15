812a17df6176178eb609813754dbe8bdf49a97b1
Callum Ackland s3791362 <s3791362@student.rmit.edu.au>
2020-08-15 11:06:10 +1000

    Homepage skeleton implementation

---
+++ FrontEnd/react-web-app/src/components/HomePage/HomePageContent.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import "../../App.scss"
+
+export default class HomePageContent extends React.Component {
+    render () {
+        return (
+            <div className="content">
+                <section className="hero is-primary">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title has-text-centered">
+                                placeholder hero
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+                <br/>
+                <div className="container is-fluid">
+                    <div className="box">test</div>
+                </div>
+            </div>
+        )
+    }
+}
