ddf30e4331d8b878520e8cd02a36832882d50258
GiacomoPirrone <giacomollpirrone@gmail.com>
2020-08-15 16:40:37 +1000

    Addition of ServiceService Entity

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/repository/ServiceRepository.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/repository/ServiceRepository.java
@@ -1,2 +1,10 @@
-package com.scrumoftheearth.springbootapi.repository;public interface ServiceRepository {
+package com.scrumoftheearth.springbootapi.repository;
+
+import com.scrumoftheearth.springbootapi.model.Service;
+import com.scrumoftheearth.springbootapi.model.Worker;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServiceRepository extends CrudRepository<Service, Long> {
+    @Override
+    Iterable<Service> findAllById(Iterable<Long> iterable);
 }
--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/service/ServiceService.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/service/ServiceService.java
@@ -1,2 +1,19 @@
-package com.scrumoftheearth.springbootapi.service;public class ServiceService {
+package com.scrumoftheearth.springbootapi.service;
+
+import com.scrumoftheearth.springbootapi.model.Service;
+import com.scrumoftheearth.springbootapi.model.Worker;
+import com.scrumoftheearth.springbootapi.repository.ServiceRepository;
+import com.scrumoftheearth.springbootapi.repository.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@org.springframework.stereotype.Service
+public class ServiceService {
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+    public Service saveOrUpdateService(Service service){
+
+        //business logic
+        return serviceRepository.save(service);
+    }
 }
