7ea5c1b944fd4f059d0cecbc4d339a1f603a5fcd
GiacomoPirrone <giacomollpirrone@gmail.com>
2020-08-15 16:35:40 +1000

    Addition of ServiceSState entity

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/ServiceSState.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/ServiceSState.java
@@ -1,2 +1,66 @@
-package com.scrumoftheearth.springbootapi.model;public class ServiceSState {
+package com.scrumoftheearth.springbootapi.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class ServiceSState {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Service service;
+    @OneToOne
+    private ServiceState serviceState;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_At;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public void setServiceState(ServiceState serviceState) {
+        this.serviceState = serviceState;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public ServiceState getServiceState() {
+        return serviceState;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public ServiceSState(){
+
+    }
 }
