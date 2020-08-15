964ecde96812db74ec0644648c7336f0d5568fd1
GiacomoPirrone <giacomollpirrone@gmail.com>
2020-08-15 16:38:36 +1000

    Addition of workerWState entity

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/WorkerWState.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/model/WorkerWState.java
@@ -1,19 +1,43 @@
 package com.scrumoftheearth.springbootapi.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Entity
 public class WorkerWState {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
     private Worker worker;
+    @OneToOne
     private WorkerState workerState;
 
     public WorkerWState() {
 
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Worker getWorker() {
+        return worker;
+    }
+
+    public void setWorker(Worker worker) {
+        this.worker = worker;
+    }
+
+    public WorkerState getWorkerState() {
+        return workerState;
+    }
+
+    public void setWorkerState(WorkerState workerState) {
+        this.workerState = workerState;
     }
 
 }
