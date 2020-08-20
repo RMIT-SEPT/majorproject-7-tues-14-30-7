c7891346f2e52439f1e0949775bf41be63af1ee4
unknown <s3658746@student.rmit.edu.au>
2020-08-20 13:37:45 +1000

    Addition of SaveService, UpdateService and findById methods

--- BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/service/ServiceService.java
+++ BackEnd/spring-boot-api/src/main/java/com/scrumoftheearth/springbootapi/service/ServiceService.java
@@ -5,15 +5,32 @@ import com.scrumoftheearth.springbootapi.model.Worker;
 import com.scrumoftheearth.springbootapi.repository.ServiceRepository;
 import com.scrumoftheearth.springbootapi.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class ServiceService {
     @Autowired
     private ServiceRepository serviceRepository;
 
-    public Service saveOrUpdateService(Service service){
+    public Service getById(long Id) throws Throwable {
+        Optional<Service> result = serviceRepository.findById(Id);
+        return result.orElseThrow(() -> {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource " + Long.toString(Id) + " Not Found!");
+        });
+    }
 
-        //business logic
+    public Service saveService(Service service) {
+
         return serviceRepository.save(service);
+    }
+
+    public Service updateService(Service oldService, Service newService){
+        oldService.setWorker(newService.getWorker());
+        oldService.setServiceSState(newService.getServiceSState());
+        oldService.setDescription(newService.getDescription());
+        return serviceRepository.save(oldService);
     }
 }
