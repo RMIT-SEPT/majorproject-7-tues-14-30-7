cbe6897b930fb6d744b96e2c33f19eddf0d0f51d
[Sovatharo Huy s3783867] <s3783867@student.rmit.edu.au>
2020-08-21 13:42:36 +1000

    Added header and changed homepage to match style

--- FrontEnd/react-web-app/src/components/Customer/CustomerHomepage.tsx
+++ FrontEnd/react-web-app/src/components/Customer/CustomerHomepage.tsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import CustomerPageHeader from './CustomerPageHeader'
 
 class CustomerHomepage extends Component {
     render() {
         return (
-            <div>
-                <h2>Customer homepage.</h2>
-                <h3>Booking history:</h3>
+            <div className="content">
+                <CustomerPageHeader/>
+                <section className="hero is-primary">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title has-text-centered">
+                                Customer Dashboard
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+                <br/>
+                <div className="container is-fluid">
+                    <div className="box">Customer Booking History:</div>
+                </div>
             </div>
         )
     }
---
+++ FrontEnd/react-web-app/src/components/Customer/CustomerPageHeader.tsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import '../../App.scss';
+
+export default class CustomerPageHeader extends React.Component {
+    render() {
+        return (
+            <div>
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                <a className="navbar-item" href="//localhost:3000">
+                </a>
+                
+                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <div className="navbar-item">
+                    <div className="field">
+                        <div className="control">
+                        <input className="text is-primary" placeholder="Search"></input>
+                        </div>
+                    </div>
+                    </div>
+                    <a className="navbar-item" href="//localhost:3000">
+                    Home
+                    </a>
+
+                    <a className="navbar-item">
+                    Placeholder
+                    </a>
+
+                    <div className="navbar-item has-dropdown is-hoverable">
+                    <a className="navbar-link">
+                        More
+                    </a>
+
+                    <div className="navbar-dropdown">
+                        <a className="navbar-item">
+                        Contact Us
+                        </a>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                    <div className="buttons">
+                        <a className="button is-primary">
+                        <strong>Sign up</strong>
+                        </a>
+                        <a className="button is-light">
+                        Log in
+                        </a>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+            </div>
+        )
+    }
+}
