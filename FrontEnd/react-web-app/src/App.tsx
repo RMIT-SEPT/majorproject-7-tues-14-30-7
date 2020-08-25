61e8aa6664cafb0e2c47f53510fb731cecf61d99
unknown <s3658746@student.rmit.edu.au>
2020-08-25 15:47:31 +1000

    Changed some tags around and played with the order (Nothing deleted)

--- FrontEnd/react-web-app/src/App.tsx
+++ FrontEnd/react-web-app/src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.scss';
 import HomePageContent from './components/HomePage/HomePageContent';
 import HomePageHeader from './components/HomePage/HomePageHeader';
-
 import Worker from './components/Worker/Worker';
+import CustomerHomepage from './components/Customer/CustomerHomepage';
+import BusinessPage from './components/businessPage/BusinessPage';
 
 function App() {
   return (
     <div>
       <HomePageHeader/>
       <HomePageContent/>
+      <CustomerHomepage/>
       <Worker/>
+      <BusinessPage/>
     </div>
-   <div>
-     <Homepage/>
-   </div>
   );
 }
 
