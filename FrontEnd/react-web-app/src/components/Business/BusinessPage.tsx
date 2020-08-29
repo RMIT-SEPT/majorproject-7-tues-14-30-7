72ddd4071574550c485e296044f7e6e4fa8017b5
Adrian <s3721092@student.rmit.edu.au>
2020-08-29 18:55:53 +1000

    Dynamic tables (Proof by Concept)
    Note: for this to work atm you need to set "strict" to false, getting info from USERAPI get by id. but needs to be changed once linking between table exist

--- FrontEnd/react-web-app/src/components/Business/BusinessPage.tsx
+++ FrontEnd/react-web-app/src/components/Business/BusinessPage.tsx
@@ -9,42 +9,64 @@ export default class BusinessPage extends React.Component<{},any>{
             businessId: 1
         }
     }
-    
+
     componentDidMount(){
-        fetch("http://localhost:8080/api/Business/findById=1")
+        var apicall = "http://localhost:8080/api/Business/findById=" + this.state.businessId
+        fetch(apicall)
             .then(response => response.json())
             .then(data => {
                 this.setState({
                     business: data
                 })
             })
+        this.populatetable();
     }
 
+    populatetable(){
+        fetch("http://localhost:8080/user/1")
+            .then(res =>{
+                res.json()
+                .then(data => {
+                    var dataArray = [];
+                    dataArray.push(data);
+                    if(dataArray.length > 0){
+                        var temp :any = "";
+
+                        dataArray.forEach((row) =>{
+                            temp += "<tr>"
+                            temp += "<td>" + row.firstName + "</td>"
+                            temp += "<td>" + row.lastName + "</td>"
+                            temp += "<td>" + row.homeAddress + "</td>"
+                            temp += "<td>" + row.phoneNumber + "</td>"
+                            temp += "<td>" + "TO BE ADDED" + "</td>"
+                            temp += "<tr>"
+                        })
+                        document.getElementById("workertable").innerHTML = temp;
+                    }
+                })
+            })
+    }
+    
     render(){
         return( 
-            <body>
-                <div className="has-text-centered is-size-1">
-                    <h1>{this.state.business.name}</h1>
+            <div>
+                <div className="has-text-centered">
+                    <h1 className="title is-1">{this.state.business.name}</h1>
                 </div>
-                <br></br>
-                <div className="is-size-4">
-                    <h3>Worker List:</h3>
-                </div>
+                <h3 className="subtitle is-4">Worker List:</h3>
                 <div className="columns">
                     <div className="column is-narrow">
                         <table className="table">
-                            <tr>
-                                <th>Name</th>
-                                <th>Number</th>
-                                <th>Address</th>
-                                <th>Email</th>  
-                            </tr>
-                            <tr>
-                                <td>Steve Bobson</td>
-                                <td>000</td>
-                                <td>123 king street</td>
-                                <td>PlzHd@pass.com</td>
-                            </tr>
+                            <thead>
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Home Address</th>
+                                    <th>Number</th>
+                                    <th>Email</th>  
+                                </tr>
+                            </thead>
+                            <tbody id="workertable"></tbody>
                         </table>
                     </div>
                     <div className="column">
@@ -53,17 +75,14 @@ export default class BusinessPage extends React.Component<{},any>{
                         <p>{this.state.business.description}</p>
                     </div>
                 </div>
-                <div className="is-size-4">
-                    <h4>Contact us:</h4>
-                </div>
-                <br></br>
+                <h4 className="subtitle is-4">Contact us:</h4>
                 <div className="is-size-6">
                     <p className="has-text-weight-bold">Address:</p>
                     <p>{this.state.business.address}</p>
                     <p className="has-text-weight-bold">Mobile Number:</p>
                     <p>{this.state.business.phoneNumber}</p>
                 </div>
-            </body>
+            </div>
         )
     }
 }
