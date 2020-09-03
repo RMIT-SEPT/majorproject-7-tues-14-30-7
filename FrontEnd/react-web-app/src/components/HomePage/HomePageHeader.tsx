068f57125bd8588f141a13a5334fcfd4f4b7085d
Callum Ackland s3791362 <s3791362@student.rmit.edu.au>
2020-09-03 20:25:45 +1000

    Added homepage header

--- FrontEnd/react-web-app/src/components/HomePage/HomePageHeader.tsx
+++ FrontEnd/react-web-app/src/components/HomePage/HomePageHeader.tsx
@@ -1,66 +1,75 @@
 import React, { Component } from 'react'
 import '../../App.scss';
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 
-export default class HomePage extends React.Component {
+export default class HomePageHeader extends React.Component {
     render() {
         return (
             <div>
-            <nav className="navbar" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                <a className="navbar-item" href="//localhost:3000">
-                </a>
-                
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-                </div>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-                <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-start">
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
                     <div className="navbar-item">
-                    <div className="field">
-                        <div className="control">
-                        <input className="text is-primary" placeholder="Search"></input>
+                        <Link to="/"><img src={require("./Images/brand.png")} width="112" height="28" alt="nav-img"/></Link>
+                    </div>
+                    
+                    <label
+                        role="button"
+                        className="navbar-burger burger"
+                        aria-label="menu"
+                        aria-expanded="false"
+                        htmlFor="nav-toggle-state"
+                        >
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                    </label>
+                    </div>
+                    <input type="checkbox" id="nav-toggle-state" />
+
+                    <div className="navbar-menu">
+                        <div className="navbar-start">
+                            <div className="navbar-item" id="burgerzoomed">
+                                <div className="field">
+                                    <div className="control">
+                                        <input className="text is-primary" placeholder="Search"></input>
+                                        <label> <a href="localhost:3000" id="searchbutton">
+                                            <i className="fa fa-search"></i></a></label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="navbar-item">
+                                <Link to="/CustomerHomePage">Dashboard</Link>
+                            </div>
+                            <div className="navbar-item">
+                                <Link to="/Worker">tempworkerlink</Link>
+                            </div>
+                            <div className="navbar-item has-dropdown is-hoverable">
+                                    <div className="navbar-link" id="dropdown">
+                                        More
+                                    </div>
+                                    <div className="navbar-dropdown">
+                                        <div className="navbar-item">
+                                        <Link to="/">Contact Us</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    <div className="navbar-end">
+                            <div className="navbar-item">
+                                <div className="buttons">
+                                    <div className="button is-primary" id="signup">
+                                        <strong>Sign up</strong>
+                                    </div>
+                                    <div className="button is-light">
+                                        Log in
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
-                    <a className="navbar-item">
-                    Dashboard
-                    </a>
-
-                    <a className="navbar-item">
-                    placeholder
-                    </a>
-
-                    <div className="navbar-item has-dropdown is-hoverable">
-                    <a className="navbar-link">
-                        More
-                    </a>
-
-                    <div className="navbar-dropdown">
-                        <a className="navbar-item">
-                        Contact Us
-                        </a>
-                    </div>
-                    </div>
-                    </div>
-                </div>
-
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                    <div className="buttons">
-                        <a className="button is-primary">
-                        <strong>Sign up</strong>
-                        </a>
-                        <a className="button is-light">
-                        Log in
-                        </a>
-                    </div>
-                    </div>
-                </div>
-            </nav>
+                </nav>
             </div>
         )
     }
