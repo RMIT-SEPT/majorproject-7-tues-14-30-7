de2dfeee66edf0805c94d45c7a1e036627ddbb79
Adrian <s3721092@student.rmit.edu.au>
2020-10-10 22:17:33 +1100

    axios post fix, (enough for demo)

--- FrontEnd/react-web-app/src/actions/registerAction.js
+++ FrontEnd/react-web-app/src/actions/registerAction.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GET_ERRORS } from "./types";
 
 export const registerAction = (newUser, newBusiness, newWorkerDetails, form, history) => async dispatch => {
-  // try {
+  try {
       if(form == 1){
             const res = await axios.post("http://localhost:8080/api/user", newUser);
             console.log(res.data.id);
@@ -10,7 +10,8 @@ export const registerAction = (newUser, newBusiness, newWorkerDetails, form, his
             history.push(`/UserHomepage/${userID}`);
       }
       if(form == 2){
-        const resbus = await axios.post("https://localhost:8080/api/Business", newBusiness);
+        console.log("here")
+        const resbus = await axios.post("http://localhost:8080/api/Business", newBusiness);
         const busID = resbus.data.id;
         console.log(busID);
 
@@ -20,7 +21,7 @@ export const registerAction = (newUser, newBusiness, newWorkerDetails, form, his
                 business_id: busID
             }
             console.log(postbusinessTime);
-            const res = await axios.post("http://localhost:8080/api/BusinessHours", postbusinessTime);
+            await axios.post("http://localhost:8080/api/BusinessHours", postbusinessTime);
         }
         console.log("end of loop");
         history.push(`/BusinessPage/${busID}`);
@@ -54,33 +55,33 @@ export const registerAction = (newUser, newBusiness, newWorkerDetails, form, his
         const workerID = res2.data.id;
         history.push(`/Worker/?id=${workerID}`);
       }
-  // } catch (err) {
-  //   dispatch({
-  //     type: GET_ERRORS,
-  //     payload: err.response.data
-  //   });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
 
-  //   console.log(err.response.data);
-  //   if(err.response.data.errors.userName != null){
-  //   alert(err.response.data.errors.userName);
-  //   }
-  //   if(err.response.data.errors.firstName != null){
-  //     alert(err.response.data.errors.firstName);
-  //   }
-  //   if(err.response.data.errors.lastName != null){
-  //     alert(err.response.data.errors.lastName);
-  //   }
-  //   if(err.response.data.errors.phoneNumber != null){
-  //     alert(err.response.data.errors.phoneNumber);
-  //   }
-  //   if(err.response.data.errors.homeAddress != null){
-  //     alert(err.response.data.errors.homeAddress);
-  //   }
-  //   if(err.response.data.errors.password != null){
-  //     alert(err.response.data.errors.password);
-  //   }
-  //   if(err.response.data.errors.passwordConfirmation != null){
-  //     alert(err.response.data.errors.passwordConfirmation);
-  //   }
-  // }
+    console.log(err.response.data);
+    if(err.response.data.errors.userName != null){
+    alert(err.response.data.errors.userName);
+    }
+    if(err.response.data.errors.firstName != null){
+      alert(err.response.data.errors.firstName);
+    }
+    if(err.response.data.errors.lastName != null){
+      alert(err.response.data.errors.lastName);
+    }
+    if(err.response.data.errors.phoneNumber != null){
+      alert(err.response.data.errors.phoneNumber);
+    }
+    if(err.response.data.errors.homeAddress != null){
+      alert(err.response.data.errors.homeAddress);
+    }
+    if(err.response.data.errors.password != null){
+      alert(err.response.data.errors.password);
+    }
+    if(err.response.data.errors.passwordConfirmation != null){
+      alert(err.response.data.errors.passwordConfirmation);
+    }
+  }
 };
